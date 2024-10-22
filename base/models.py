ecc7c877e703c3c546fa2cdfb3b946983cb7561c
dgusfr <diegogustavo.fr@gmail.com>
2024-10-22 13:27:24 -0300

    feat(refact): refact the models base

--- base/models.py
+++ base/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 class Item(models.Model):
-   name = models.CharField(max_length=200)
-   created = models.DateTimeField(auto_now_add = True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
