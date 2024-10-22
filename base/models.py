a0d14cc31f88de17c1201f7d3a8f666d53073612
dgusfr <diegogustavo.fr@gmail.com>
2024-10-22 13:30:36 -0300

    make migrations for models

--- base/models.py
+++ base/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    category = models.CharField(max_length=100)
-    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    description = models.TextField(default="Sem descrição")
+    category = models.CharField(max_length=100, default="Geral")
+    stock = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
