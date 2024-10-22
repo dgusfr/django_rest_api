b265d373fe4e42aa85f6bc2f2377ae932e1d2d3b
dgusfr <diegogustavo.fr@gmail.com>
2024-10-22 13:20:10 -0300

    feat(urls): in new method

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-  path('', views.getData),
-  path('add/', views.addItem)
+    path('', views.getData),
+    path('add/', views.addItem),
+    path('delete/<int:pk>/', views.deleteItem),  
+    path('update/<int:pk>/', views.updateItem), 
 ]
