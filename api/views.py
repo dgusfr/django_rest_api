b8c97ba662bc0f76100c43eb4736d13f47723d30
dgusfr <diegogustavo.fr@gmail.com>
2024-10-22 13:18:28 -0300

    feat(add): add new methods

--- api/views.py
+++ api/views.py
@@ -1,18 +1,45 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from base.models import Item
 from .serializers import ItemSerializer
 
-
 @api_view(['GET'])
 def getData(request):
-  items = Item.objects.all()
-  serializer = ItemSerializer(items, many=True)
-  return Response(serializer.data)
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def addItem(request):
-  serializer = ItemSerializer(data=request.data)
-  if serializer.is_valid():
-    serializer.save()
-  return Response(serializer.data)
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def deleteItem(request, pk):
+    try:
+        item = Item.objects.get(id=pk)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Item.DoesNotExist:
+        return Response({'error': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PATCH', 'PUT'])
+def updateItem(request, pk):
+    try:
+        item = Item.objects.get(id=pk)
+    except Item.DoesNotExist:
+        return Response({'error': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PATCH':
+        serializer = ItemSerializer(item, data=request.data, partial=True)
+    else:
+        serializer = ItemSerializer(item, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
